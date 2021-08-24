897d9e9ddfe00d88a2df7d01a8a5ce8f2e4214db
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-24 13:18:09 -0300

    testando build com target app, lib e wc

---
+++ secao-5/vue-cli-hello-world/OlaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <input type="text" v-model="titulo" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: "Olá Vuejs",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
