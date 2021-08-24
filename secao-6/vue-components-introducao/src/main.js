3bb5b5f7964d7adb8d04fe32f4472086eb6af20c
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-24 15:01:01 -0300

    registrando componente global e local

--- secao-6/vue-components-introducao/src/main.js
+++ secao-6/vue-components-introducao/src/main.js
@@ -1,5 +1,57 @@
-import Vue from 'vue'
+import Vue from 'vue';
+
+// const data = {
+//   titulo: 'Curso de VueJS',
+//   contador: 0,
+// };
+
+/*Vue.component('pjn-component', {
+  data: function() {
+    return {
+      titulo: 'Curso de VueJS',
+      contador: 0,
+    }
+  },
+  methods: {
+    incrementar: function () {
+      this.contador++;
+    }
+  },
+  template: `
+    <div>
+      <h1>{{ titulo }}</h1>
+      <button @click="incrementar">Clicado {{ contador }}</button>
+    </div>
+  `
+});*/
+
+const pjnComponent = {
+  data: function() {
+    return {
+      titulo: 'Curso de VueJS',
+      contador: 0,
+    }
+  },
+  methods: {
+    incrementar: function () {
+      this.contador++;
+    }
+  },
+  template: `
+    <div>
+      <h1>{{ titulo }}</h1>
+      <button @click="incrementar">Clicado {{ contador }}</button>
+    </div>
+  `
+};
 
 new Vue({
+  el: '#app1',
+  components: {
+    'pjn-component': pjnComponent,
+  }
+});
 
-}).$mount('#app')
+new Vue({
+  el: '#app2',
+});
