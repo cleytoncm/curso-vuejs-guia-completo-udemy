6699f0f0e6b5010bb5a993f3867b47f80ca4fe48
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-24 15:10:53 -0300

    criando novo componente

--- secao-6/vue-components-introducao/src/main.js
+++ secao-6/vue-components-introducao/src/main.js
@@ -1,57 +1,9 @@
 import Vue from 'vue';
+// import AppContador from './AppContador';
+import App from './App';
 
-// const data = {
-//   titulo: 'Curso de VueJS',
-//   contador: 0,
-// };
-
-/*Vue.component('pjn-component', {
-  data: function() {
-    return {
-      titulo: 'Curso de VueJS',
-      contador: 0,
-    }
-  },
-  methods: {
-    incrementar: function () {
-      this.contador++;
-    }
-  },
-  template: `
-    <div>
-      <h1>{{ titulo }}</h1>
-      <button @click="incrementar">Clicado {{ contador }}</button>
-    </div>
-  `
-});*/
-
-const pjnComponent = {
-  data: function() {
-    return {
-      titulo: 'Curso de VueJS',
-      contador: 0,
-    }
-  },
-  methods: {
-    incrementar: function () {
-      this.contador++;
-    }
-  },
-  template: `
-    <div>
-      <h1>{{ titulo }}</h1>
-      <button @click="incrementar">Clicado {{ contador }}</button>
-    </div>
-  `
-};
+// Vue.component('app-contador', AppContador);
 
 new Vue({
-  el: '#app1',
-  components: {
-    'pjn-component': pjnComponent,
-  }
-});
-
-new Vue({
-  el: '#app2',
-});
+    render: h => h(App),
+}).$mount('#app');
