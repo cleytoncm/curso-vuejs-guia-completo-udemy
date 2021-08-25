1a161be82658c697c0e43896294e07e59788560e
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-25 15:16:10 -0300

    customizando template com slot

--- secao-8/vue-components-avancado/src/App.vue
+++ secao-8/vue-components-avancado/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
-    <h1>Vue Components</h1>
+    <h1>VueJS</h1>
+    <Post titulo="Components no Vue">
+      <h2>Components no Vue</h2>
+      <p>Compopnents são uma das peças mais importantes no Vue</p>
+    </Post>
   </div>
 </template>
+
+<script>
+  import Post from './components/Post';
+
+  export default {
+    components: {
+      Post
+    }
+  }
+</script>
