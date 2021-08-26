194ae3131fc8a958d90768ad312e9270f1bed7c1
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-26 13:26:07 -0300

    iniciando projeto

---
+++ secao-10/vue-animacoes/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinamento transição/animação de elementos/components no Vue.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
+      <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mostrar: true,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
