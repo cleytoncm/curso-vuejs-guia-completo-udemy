1dc535917ddb53672aa60ed407c8b2dbb719c39c
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-27 19:11:51 -0300

    preparando código para animas números

--- secao-10/vue-animacoes/src/App.vue
+++ secao-10/vue-animacoes/src/App.vue
@@ -8,92 +8,30 @@
     </div>
 
     <div class="container">
-      <h3 class="font-weight-light">Tecnologias</h3>
+      <h3 class="font-weight-light">Animações de estado</h3>
 
-      <div class="row">
-        <div class="col-sm-2">
-          <button class="btn btn-info" @click="embaralhar">Emparalhar</button>
-        </div>
-        <div class="col-sm-10">
-          <div class="form-group">
-            <input
-                type="text"
-                class="form-control"
-                placeholder="Insira um novo item e pressione Enter"
-                @keyup.enter="adicionar"
-                ref="input"
-            >
-          </div>
-        </div>
+      <div class="form-group">
+        <input class="form-control" v-model="numero" step="50">
       </div>
 
-      <transition-group tag="ul" class="list-group" name="list">
-        <li
-            class="list-group-item"
-            v-for="(tecnologia, indice) in tecnologias"
-            :key="tecnologia"
-        >
-          <span>{{ tecnologia }}</span>
-          <button
-              class="btn btn-danger btn-sm float-end"
-              @click="remover(indice)"
-          >
-            x
-          </button>
-        </li>
-      </transition-group>
+      <div class="alert alert-info">
+        <h3 class="font-weight-light">
+          <strong>Número: </strong>
+          <span>{{ numero }}</span>
+        </h3>
+      </div>
 
     </div>
   </div>
 </template>
 
 <script>
-import { shuffle } from 'lodash';
 export default {
   data() {
     return {
-      tecnologias: [
-        'Javascript',
-        'Vue JS',
-        'Vuex',
-        'Vue Router',
-      ],
-    }
-  },
-  methods: {
-    adicionar(event) {
-      const novoItem = event.target.value;
-      if (novoItem) {
-        const indice = Math.floor(Math.random() * this.tecnologias.length);
-        this.tecnologias.splice(indice, 0, novoItem);
-        this.$refs.input.value = '';
-      }
-    },
-    remover(indice) {
-      this.tecnologias.splice(indice, 1);
-    },
-    embaralhar() {
-      this.tecnologias = shuffle(this.tecnologias);
+      numero: 0,
     }
   },
 }
 </script>
 
-
-
-<style scoped>
-  .list-enter, .list-leave-to {
-    opacity: 0;
-    transform: translateX(-70px);
-  }
-
-  .list-enter-active, .list-leave-active, .list-move {
-    transition: all 1s;
-  }
-
-  .list-leave-active {
-    position: absolute;
-    width: calc(100% - 100px);
-  }
-
-</style>
