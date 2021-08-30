615828550da4cd1c8bfc56fa536baec2af4b5e1e
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 12:04:51 -0300

    navegando via router-link

--- secao-11/vue-rotas/src/App.vue
+++ secao-11/vue-rotas/src/App.vue
@@ -6,6 +6,10 @@
     </div>
 
     <div class="container">
+
+      <router-link to="/" class="btn btn-info mb-2 me-2">Home</router-link>
+      <router-link to="/contatos" class="btn btn-info mb-2 me-2">Contatos</router-link>
+
       <router-view></router-view>
     </div>
   </div>
