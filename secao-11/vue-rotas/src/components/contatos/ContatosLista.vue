fe99fca08dd624c50c94ff86cca1bd6a101c4351
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 18:59:27 -0300

    usando query params

--- secao-11/vue-rotas/src/components/contatos/ContatosLista.vue
+++ secao-11/vue-rotas/src/components/contatos/ContatosLista.vue
@@ -2,10 +2,21 @@
   <div>
     <h3 class="font-weight-light">Contatos</h3>
 
-    <ul class="list-group" v-if="contatos.length > 0">
+    <div class="form-group">
+      <input
+          type="search"
+          class="form-control"
+          @keyup.enter="buscar"
+          :value="$route.query.busca"
+      >
+    </div>
+
+    <hr>
+
+    <ul class="list-group" v-if="contatosFiltrados.length > 0">
       <ContatosListaItem
           class="list-group-item"
-          v-for="contato in contatos"
+          v-for="contato in contatosFiltrados"
           :key="contato.id"
           :contato="contato"
       />
@@ -34,7 +45,22 @@ export default {
       ]
     }
   },
+  computed: {
+    contatosFiltrados() {
+      const busca = this.$route.query.busca;
+
+      return !busca ? this.contatos : this.contatos.filter(c => c.nome.toLowerCase().includes(busca.toLowerCase()));
+    },
+  },
   methods: {
+    buscar(event) {
+      this.$router.push({
+        path: '/contatos',
+        query: {
+          busca: event.target.value
+        }
+      });
+    },
     voltar() {
       this.$router.back();
     },
