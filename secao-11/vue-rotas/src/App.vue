9770598f7ee3ebeb8524f1001ab04824c76348d9
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 11:44:50 -0300

    criando projeto e configurando o vue router

---
+++ secao-11/vue-rotas/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <h1 class="display-4">Vue Router</h1>
+      <p class="lead">Adicionando rotas a Single Page Applications no Vue.</p>
+    </div>
+
+    <div class="container">
+      <h3 class="font-weight-light">Contatos</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
