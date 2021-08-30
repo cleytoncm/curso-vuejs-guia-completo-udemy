d197481bee4c263bb044688bc1a7780fbf53d098
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 12:20:51 -0300

    aplicando classe css no router-link ativo

--- secao-11/vue-rotas/src/App.vue
+++ secao-11/vue-rotas/src/App.vue
@@ -7,8 +7,19 @@
 
     <div class="container">
 
-      <router-link to="/" class="btn btn-info mb-2 me-2">Home</router-link>
-      <router-link to="/contatos" class="btn btn-info mb-2 me-2">Contatos</router-link>
+      <router-link
+          to="/"
+          class="btn btn-info mb-2 me-2"
+          exact
+      >
+        Home
+      </router-link>
+      <router-link
+          :to="{ path: '/contatos' }"
+          class="btn btn-info mb-2 me-2"
+      >
+        Contatos
+      </router-link>
 
       <router-view></router-view>
     </div>
