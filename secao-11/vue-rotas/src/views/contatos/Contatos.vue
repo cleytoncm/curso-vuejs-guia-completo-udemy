75db1039b7ce703f0a22a6745a403a1756910779
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 12:25:14 -0300

    navegando de forma programatica

--- secao-11/vue-rotas/src/views/contatos/Contatos.vue
+++ secao-11/vue-rotas/src/views/contatos/Contatos.vue
@@ -1,10 +1,24 @@
 <template>
-  <h3 class="font-weight-light">Contatos</h3>
+  <div>
+    <h3 class="font-weight-light">Contatos</h3>
+    <button class="btn btn-secondary mt-4 mb-4" @click="voltar">Voltar</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Contatos"
+  name: "Contatos",
+  methods: {
+    voltar() {
+        // this.$router.push({ path: '/' });
+        // this.$router.push('/');
+        // this.$router.replace('/');
+        // this.$router.go(-1);
+        // this.$router.forward();
+      this.$router.back();
+    },
+
+  },
 }
 </script>
 
