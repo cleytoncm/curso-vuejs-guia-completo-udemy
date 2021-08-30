11d38d2885a6a5ebd1bdc4a456483ce566ce83e3
Cleyton de Castro <cleytoncm@gmail.com>
2021-08-30 15:50:43 -0300

    capturando parametros das rotas

--- secao-11/vue-rotas/src/views/contatos/ContatoDetalhe.vue
+++ secao-11/vue-rotas/src/views/contatos/ContatoDetalhe.vue
@@ -1,10 +1,13 @@
 <template>
-  <h3 class="font-weight-light">Detalhes sobre o contato</h3>
+  <h3 class="font-weight-light">Detalhes sobre o Contato com id: {{ $route.params.id }}</h3>
 </template>
 
 <script>
 export default {
-  name: "ContatoDetalhe"
+  name: "ContatoDetalhe",
+  created() {
+    console.log('Parâmetros da rota: ', this.$route.params);
+  }
 }
 </script>
 
