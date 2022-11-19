9d0766e652f6febf956538f5ee53a8f9a140a6e5
Cleyton de Castro <cleytoncm@gmail.com>
2022-11-19 16:13:46 -0300

    Usando Guarda de Rota "beforeRouteUpdate" (No Component)

--- secao-11/vue-rotas/src/views/contatos/ContatoDetalhes.vue
+++ secao-11/vue-rotas/src/views/contatos/ContatoDetalhes.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3 class="font-weight-light">Detalhes sobre o Contato com id: {{ id }}</h3>
+    <p>Paramêttrtos: {{ parametros }}</p>
     <router-link
         :to="`/contatos/${id}/editar`"
         class="btn btn-primary"
@@ -19,9 +20,19 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      parametros: this.$route.params,
+    }
+  },
   created() {
     console.log('Parâmetros da rota: ', this.$props);
   },
+  beforeRouteUpdate(to, from, next) {
+    console.log('beforeRouteUpdate');
+    this.parametros = to.params;
+    next();
+  }
   // watch: {
   //   // eslint-disable-next-line no-unused-vars
   //   $route(to, from) {
